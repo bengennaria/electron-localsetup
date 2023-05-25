238a3b378dfc2eb31fe1325fc7bbae0a708ee240
Ben Gennaria <ben@gennaria.com>
2023-05-25 14:02:08 -0500

    initial port from npm

---
+++ README.md
@@ -0,0 +1,3 @@
+# electron-localsetup
+
+Install and launch Electron app packages using the default OS program folder – '/Applications' on macOS, 'C:\\Program Files' on Windows and '/usr/local' on Linux
---
+++ index.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+'use strict'
+
+
+/**
+ * Modules
+ * External
+ * @constant
+ */
+const logger = require('@sidneys/logger')({ timestamp: false })
+const minimist = require('minimist')
+
+/**
+ * Modules
+ * Internal
+ * @constant
+ */
+const setupApplicationLocally = require('./lib/setup-application-locally')
+const runBuild = require('./lib/run-build')
+
+
+/**
+ * Main
+ * @namespace process.env.npm_config_argv.original
+ */
+if (require.main === module) {
+    /**
+     * Parse Arguments
+     */
+    let argv
+
+    try {
+        argv = minimist(JSON.parse(process.env.npm_config_argv).original, {
+            'boolean': [
+                'build'
+            ],
+            'unknown': () => { return false }
+        })
+    } catch (error) {
+        logger.error('could not parse arguments', error)
+    }
+
+    /**
+     * Resolve Arguments
+     */
+
+    // --build
+    let argvBuild = argv['build']
+
+    // DEBUG
+    logger.debug('argv', argv)
+
+    /**
+     * Build externally
+     */
+    if (argvBuild) {
+        runBuild()
+    }
+
+    /**
+     * Start local application setup
+     */
+    setupApplicationLocally((error) => {
+        if (error) {
+            logger.error(error)
+            process.exit(1)
+        }
+
+        process.exit(0)
+    })
+}
